2822bb99ed2f891959794c53aeab76b55b097373
Arnab Chowdhury <arnabchowdhury1156@gmail.com>
2022-10-18 01:35:11 +0530

    printArray function was added

--- insertionSort.c
+++ insertionSort.c
@@ -18,6 +18,14 @@ void insertionSort(int arr[], int n)
     }
 }
 
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
 int main()
 {
     int arr[] = {12, 11, 13, 5, 6};
